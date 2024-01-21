22275a757262ca17ec2b0bce20ca3b937818de8e
Braden Helmer <bradenhelmeraus@gmail.com>
2024-01-21 10:55:40 -0500

    bug fixes

--- lua/nvim-syncer/config.lua
+++ lua/nvim-syncer/config.lua
@@ -1,8 +1,8 @@
 -- config.lua
 -- ~~~~~~~~~~
 -- Handlers for getting config
-local config = {opts = nil}
-function config.get_config_file()
+local config = {}
+local function get_config_file()
 	local file_path = vim.fn.findfile("sync.lua")
 	if string.len(file_path) > 0 then
 		config.opts = require("sync")
@@ -12,8 +12,6 @@ function config.get_config_file()
 	return false
 end
 
-if config.get_config_file() then
+if get_config_file() then
 	return config
-else
-	return nil
 end
--- lua/nvim-syncer/init.lua
+++ lua/nvim-syncer/init.lua
@@ -3,27 +3,25 @@
 -- Plugin Routing
 local M = {}
 local config = require('nvim-syncer.config')
--- If config is null, we know there is no sync.lua file, just return
-if config == nil then
-	return nil
-end
 local syncer = require("nvim-syncer.syncer")
 
 
--- Syncer autocommands
-vim.api.nvim_create_user_command("SyncUp", function()
-	syncer.sync_up(config.opts)
-end, {})
+-- Conditionally create commands if config present
+if config ~= true then
+	-- Syncer autocommands
+	vim.api.nvim_create_user_command("SyncUp", function()
+		syncer.sync_up(config.opts)
+	end, {})
 
-vim.api.nvim_create_user_command("SyncDown", function()
-	syncer.sync_down(config.opts)
-end, {})
-
--- Conditionally create autocommand to sync on save.
-if config.opts.on_save then
-	vim.api.nvim_create_autocmd({"BufWritePost"}, {
-		callback = function() syncer.sync_up(config.opts) end
-	})
+	vim.api.nvim_create_user_command("SyncDown", function()
+		syncer.sync_down(config.opts)
+	end, {})
+	 -- Sync on save
+	if config.opts.on_save then
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function() syncer.sync_up(config.opts) end
+		})
+	end
 end
 
 return M
