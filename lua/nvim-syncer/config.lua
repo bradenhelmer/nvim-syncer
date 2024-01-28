d1cd1c1f8816ed45f1bfb18d52c1b040a3f406de
Braden Helmer <bradenhelmeraus@gmail.com>
2024-01-28 06:38:03 -0500

    Fix require bug

--- lua/nvim-syncer/config.lua
+++ lua/nvim-syncer/config.lua
@@ -3,10 +3,11 @@
 -- Handlers for getting config
 local config = {}
 local function get_config_file()
-	local file_path = vim.fn.findfile("sync.lua")
+	local src_path = vim.fn.getcwd()
+	local file_path = vim.fn.findfile(src_path .. '/' .. "sync.lua")
 	if string.len(file_path) > 0 then
 		config.opts = require("sync")
-		config.opts.src_path = vim.fn.getcwd()
+		config.opts.src_path = src_path
 		return true
 	end
 	return false
